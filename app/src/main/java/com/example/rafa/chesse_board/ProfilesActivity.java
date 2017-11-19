cf1db618dc0893af7e012b8954eedf7c84322b58
Rafael Filipe Carreira Henriques <henriques.rafa@hotmail.com>
2017-11-19 17:10:40 +0000

    Added Start Activity

---
+++ app/src/main/java/com/example/rafa/chesse_board/ProfilesActivity.java
@@ -0,0 +1,13 @@
+package com.example.rafa.chesse_board;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfilesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profiles);
+    }
+}
